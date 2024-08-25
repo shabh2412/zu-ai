c93ed5159e2f23fc35f31154dff0292d98212ccd
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-25 20:19:43 +0530

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body className={inter.className}>
+        {/* sidebar - navbar - with hamburger in small screen */}
+        {/* contents in the below div */}
+        <div className="px-3">{children}</div>
+      </body>
     </html>
   );
 }
