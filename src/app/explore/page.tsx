c6aca369cafbcf2f8cdd100ea873b40ee93630b2
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-25 20:20:44 +0530

    create explore page

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import GreetingMessage from "./GreetingMessage";
+import InputSection from "./InputSection";
+
+function Explore() {
+  const primaryMessage =
+    "Hey IB Folks ! Unsure about the quality of your answers?";
+  const secondaryMessage = "We get you.";
+  return (
+    <main className="pt-8 flex flex-col gap-4">
+      <GreetingMessage primary={primaryMessage} secondary={secondaryMessage} />
+      <InputSection />
+    </main>
+  );
+}
+
+export default Explore;
