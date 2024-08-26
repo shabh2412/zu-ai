b260ff4c7f95ed2038a2f5dbe046c18b5ae9ccf5
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-26 13:16:23 +0530

    add: create action to make api call to evaluate pdf

---
+++ src/app/explore/actions.ts
@@ -0,0 +1,35 @@
+import { saveFileToLocalStorage } from "@/app/explore/utils/";
+import { FileState } from "./interface";
+
+export async function evaluate(file: FileState) {
+  try {
+    const formData = new FormData();
+    formData.append("fileUrl", file.fileUrl as string);
+    formData.append("courseworkType", file.courseworkType as string);
+    formData.append("subject", file.subject as string);
+    formData.append("title", file.title);
+    formData.append("file", file.file as File);
+    // make a post api call to the route.ts
+    const response = await fetch("/explore/api", {
+      method: "POST",
+      body: formData,
+    })
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    // save the file to localStorage
+    const result = await response.json();
+    saveFileToLocalStorage({ ...result, file: file.file as File });
+    // create a promise and save the file after 2 seconds
+    // const promise = new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve(saveFileToLocalStorage(file));
+    //   }, 2000);
+    // })
+
+    // wait for the promise to resolve
+    // await promise;
+  } catch (error) {
+    console.error("Yo! Error saving file to localStorage:", error);
+  }
+}
