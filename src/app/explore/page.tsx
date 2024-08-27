4fef76fcbebc39d1941beef0a97cbaeb0f169904
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-28 02:44:28 +0530

    add: complete explore page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import GreetingMessage from "./GreetingMessage";
 import InputSection from "./InputSection";
 import Image from "next/image";
+import MyCourseworkGrid from "./CourseworkGrid";
+import ExploreCoursework from "./ExploreCoursework";
 
 function Explore() {
   const primaryMessage =
@@ -9,26 +11,30 @@ function Explore() {
   const secondaryMessage = "We get you.";
 
   return (
-    <main className="flex gap-4 justify-center w-full">
-      <div className="flex flex-col gap-4 md:mx-auto md:max-w-[500px] lg:mx-0">
-        <GreetingMessage
-          primary={primaryMessage}
-          secondary={secondaryMessage}
-        />
-        <Suspense>
-          <InputSection />
-        </Suspense>
+    <main className="flex flex-col gap-8">
+      <div className="flex gap-4 justify-center w-full">
+        <div className="flex flex-col gap-4 md:mx-auto md:max-w-[500px] lg:mx-0">
+          <GreetingMessage
+            primary={primaryMessage}
+            secondary={secondaryMessage}
+          />
+          <Suspense>
+            <InputSection />
+          </Suspense>
+        </div>
+        {/* input screen asset comes here */}
+        <div className="w-fit hidden lg:block h-full self-end">
+          <Image
+            src="/input_screen_asset.svg"
+            alt="explore"
+            className="object-cover"
+            width={290}
+            height={528}
+          />
+        </div>
       </div>
-      {/* input screen asset comes here */}
-      <div className="w-fit hidden lg:block h-full self-end">
-        <Image
-          src="/input_screen_asset.svg"
-          alt="explore"
-          className="object-cover"
-          width={290}
-          height={528}
-        />
-      </div>
+      <MyCourseworkGrid title="My coursework" />
+      <ExploreCoursework />
     </main>
   );
 }
