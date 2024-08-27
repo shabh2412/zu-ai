491ade425128ec608f1bb4865131a26cf32844e3
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-27 16:01:57 +0530

    update: responsive styling for medium and large screen devices

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -8,11 +8,24 @@ function Explore() {
   const secondaryMessage = "We get you.";
 
   return (
-    <main className="pt-8 flex flex-col gap-4 md:mx-auto md:max-w-[500px]">
-      <GreetingMessage primary={primaryMessage} secondary={secondaryMessage} />
-      <Suspense>
-        <InputSection />
-      </Suspense>
+    <main className="flex gap-4 justify-center w-full">
+      <div className="pt-8 flex flex-col gap-4 md:mx-auto md:max-w-[500px] lg:mx-0">
+        <GreetingMessage
+          primary={primaryMessage}
+          secondary={secondaryMessage}
+        />
+        <Suspense>
+          <InputSection />
+        </Suspense>
+      </div>
+      {/* input screen asset comes here */}
+      <div className="w-fit hidden lg:block h-full self-end">
+        <img
+          src="/input_screen_asset.svg"
+          alt="explore"
+          className="object-cover"
+        />
+      </div>
     </main>
   );
 }
