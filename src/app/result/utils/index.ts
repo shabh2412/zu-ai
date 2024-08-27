304a14db1c8bb5197a4a0e36f77743afaaafdead
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-27 16:46:20 +0530

    add: populate initial data

--- src/app/result/utils/index.ts
+++ src/app/result/utils/index.ts
@@ -1,8 +1,14 @@
 import { CourseWorkWithFile } from "@/app/explore/interface";
+import jsonData from "@/stores/data/index.json";
 
 export const getSavedFiles = (): CourseWorkWithFile[] => {
   try {
     const savedFiles = localStorage.getItem("savedFiles");
+    if (!savedFiles) {
+      const data = jsonData as CourseWorkWithFile[];
+      localStorage.setItem("savedFiles", JSON.stringify(data));
+      return data;
+    }
     return savedFiles ? JSON.parse(savedFiles) as CourseWorkWithFile[] : [];
   } catch (error) {
     console.error("Error parsing saved files from localStorage:", error);
