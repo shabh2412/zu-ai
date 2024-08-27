00a3aae2c59e090a310a45d44a678aec29b9382f
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-27 11:19:47 +0530

    add: dynamic result page

--- src/app/result/[result_id]/page.tsx
+++ src/app/result/[result_id]/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import { CourseWorkWithFile } from "@/app/explore/interface";
+import { useEffect, useState } from "react";
+import PdfViewer from "./PdfViewer";
+import { cn } from "@/lib/utils";
+import Header from "./Header";
+import { Button } from "@/components/ui/button";
+import { getSavedFiles } from "../utils";
+import Card from "./Card";
+import { useCourseworkStore } from "@/providers/coursework-store-provider";
+
 export default function Page({ params }: { params: { result_id: string } }) {
-  return <div>Result {params.result_id}</div>;
+  const { courseworks, initializeCourseworks } = useCourseworkStore(
+    (state) => state
+  );
+
+  useEffect(() => {
+    if (courseworks.length === 0) {
+      initializeCourseworks(getSavedFiles());
+    }
+  }, []);
+
+  const currentFile =
+    courseworks.find((file) => file.id === params.result_id) || null;
+
+  return (
+    <div className="w-full flex flex-col gap-6 px-3 py-6">
+      {/* Overall score */}
+      {currentFile && <Header currentFile={currentFile} />}
+      <Button></Button>
+      {currentFile ? (
+        <div>
+          {/* Render other details of the CourseWork */}
+          <PdfViewer
+            base64File={currentFile.base64File}
+            fileName={currentFile.fileName}
+          />
+          {/* <p>{currentFile.base64File}</p> */}
+        </div>
+      ) : (
+        <p>No result found.</p>
+      )}
+    </div>
+  );
 }
