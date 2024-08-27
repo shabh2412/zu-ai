c43c418d14fc77ee93792bccfc4af4d74842d5c1
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-08-27 11:09:14 +0530

    add: update zustand state of coursework.

--- src/app/explore/actions.ts
+++ src/app/explore/actions.ts
@@ -1,7 +1,7 @@
-import { saveFileToLocalStorage } from "@/app/explore/utils/";
-import { FileState } from "./interface";
+import { fileToBase64, saveFileToLocalStorage } from "@/app/explore/utils/";
+import { CourseWork, CourseWorkWithFile, FileState } from "./interface";
 
-export async function evaluate(file: FileState) {
+export async function evaluate(file: FileState, addCoursework: (coursework: CourseWorkWithFile) => void) {
   try {
     const formData = new FormData();
     formData.append("fileUrl", file.fileUrl as string);
@@ -9,6 +9,7 @@ export async function evaluate(file: FileState) {
     formData.append("subject", file.subject as string);
     formData.append("title", file.title);
     formData.append("file", file.file as File);
+    formData.append("author", "John Doe");
     // make a post api call to the route.ts
     const response = await fetch("/explore/api", {
       method: "POST",
@@ -20,15 +21,14 @@ export async function evaluate(file: FileState) {
     // save the file to localStorage
     const result = await response.json() as CourseWork;
     saveFileToLocalStorage({ ...result, file: file.file as File });
-    // create a promise and save the file after 2 seconds
-    // const promise = new Promise((resolve, reject) => {
-    //   setTimeout(() => {
-    //     resolve(saveFileToLocalStorage(file));
-    //   }, 2000);
-    // })
 
-    // wait for the promise to resolve
-    // await promise;
+    addCoursework({
+      ...result,
+      base64File: await fileToBase64(file.file as File),
+    });
+
+    return result;
+
   } catch (error) {
     console.error("Yo! Error saving file to localStorage:", error);
     throw error;
